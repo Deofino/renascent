d4e1c99044c86968e34ce208467f310bf81081d6
gDelps <guilhermedelfino25@gmail.com>
2020-12-03 22:14:56 -0300

    hosteado

--- classes/Conexao.php
+++ classes/Conexao.php
@@ -3,7 +3,9 @@
         public static function getConexao(){
             try {
                 //arruma aqui pq o sql que gerei ta como db nao bd
-                $conexao = new PDO("mysql:host=localhost;dbname=dbrenascent","root","");
+                //$conexao = new PDO("mysql:host=localhost;dbname=dbrenascent","root","");
+                $conexao = new PDO("mysql:host=sql111.epizy.com;dbname=epiz_27377776_dbrenascent",
+                "epiz_27377776", "wYzbO0BgwZ");
                 return $conexao;
             } catch (Exception $e) {
                 echo ($e);
